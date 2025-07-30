f496a375952c175dbd114ed05dc34b2ac4e8393d
leoQUENETTE <leoettag@gmail.com>
2025-07-30 10:28:14 +0200

    Résolution du problème de calendrier

--- codeAndroid/app/build.gradle.kts
+++ codeAndroid/app/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android) // ✅ ajoute ce plugin
 }
 
 android {
     namespace = "com.example.todolist"
-    compileSdk = 34 // ✅ 36 n'existe pas encore (max = 34 en juillet 2025)
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.todolist"
         minSdk = 24
-        targetSdk = 34 // ✅ idem, 36 est invalide actuellement
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,9 +31,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
@@ -42,7 +38,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.okhttp3.okhttp)
-    implementation(libs.material_calendarview)
+    implementation("com.applandeo:material-calendar-view:1.9.2")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
