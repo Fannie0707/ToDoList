d7987eb0b13c4b1bc2ef3082011d6969fdbb9615
leoQUENETTE <leoettag@gmail.com>
2025-07-30 09:57:18 +0200

    Message d'explication pour les callbacks

--- codeAndroid/app/src/main/java/com/example/todolist/back/bdd/SupabaseCallback.java
+++ codeAndroid/app/src/main/java/com/example/todolist/back/bdd/SupabaseCallback.java
@@ -1,5 +1,6 @@
 package com.example.todolist.back.bdd;
-
+/** Interface permettant la mise de définir les actions a effectué après la récupération des informations via l'API REST. <br>
+ * Les deux fonctions à gérer les cas de réussite ou d'échecs lors de la récupération des informations**/
 public interface SupabaseCallback {
     void onSuccess(String json);
     void onError(Exception e);
